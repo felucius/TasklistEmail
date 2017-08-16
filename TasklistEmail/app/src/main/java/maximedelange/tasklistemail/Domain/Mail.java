81dc1dd912ddda6b0da9c0211f85bbe8398a4d57
Maxime <maximedelange1991@hotmail.com>
2017-08-16 23:21:53 +0200

    Added 3 jar files for mail access

---
+++ TasklistEmail/app/src/main/java/maximedelange/tasklistemail/Domain/Mail.java
@@ -0,0 +1,8 @@
+package maximedelange.tasklistemail.Domain;
+
+/**
+ * Created by M on 8/16/2017.
+ */
+
+public class Mail{
+}
--- TasklistEmail/app/src/main/java/maximedelange/tasklistemail/Screen/StartScreen.java
+++ TasklistEmail/app/src/main/java/maximedelange/tasklistemail/Screen/StartScreen.java
@@ -1,4 +1,4 @@
-package maximedelange.tasklistemail;
+package maximedelange.tasklistemail.Screen;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -11,6 +11,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+
+import maximedelange.tasklistemail.R;
 
 public class StartScreen extends AppCompatActivity {
 
