e3e34be777a162fbec06b25ea0cdd5a98f8f077d
Maxime <maximedelange1991@hotmail.com>
2017-08-16 19:35:09 +0200

    Sending emails with email chooser possible

--- TasklistEmail/app/src/main/java/maximedelange/tasklistemail/StartScreen.java
+++ TasklistEmail/app/src/main/java/maximedelange/tasklistemail/StartScreen.java
@@ -1,5 +1,7 @@
 package maximedelange.tasklistemail;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class StartScreen extends AppCompatActivity {
+
+    // Fields
+    private Button addTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +24,8 @@ public class StartScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Additions
+        addTask();
     }
 
     @Override
@@ -48,5 +48,30 @@ public class StartScreen extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void addTask(){
+        addTask = (Button) findViewById(R.id.btnAddTask);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] TO = {"maximedelange1991@hotmail.com"};
+                String[] CC = {""};
+
+                Intent sendEmail = new Intent(Intent.ACTION_SEND);
+                //sendEmail.setData(Uri.parse("mailto:"));
+                sendEmail.setType("message/rfc822");
+                sendEmail.putExtra(Intent.EXTRA_EMAIL, TO);
+                //sendEmail.putExtra(Intent.EXTRA_CC, CC);
+                sendEmail.putExtra(Intent.EXTRA_SUBJECT, "Test subject");
+                sendEmail.putExtra(Intent.EXTRA_TEXT, "Test message");
+
+                try {
+                    startActivity(Intent.createChooser(sendEmail, "Send email..."));
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
     }
 }
